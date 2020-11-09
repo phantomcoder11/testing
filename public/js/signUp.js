3788e7cd56729a30bd71ed764a97018b45d9fd4c
phantomcoder11 <dhritirajborah2020@gmai.com>
2020-11-10 00:12:04 +0530

    message

---
+++ public/js/signUp.js
@@ -0,0 +1,55 @@
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+const confirmPassword= document.querySelector('#confirmPassword');
+const submit = document.querySelector('#submit');
+togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
+submit.addEventListener('click', function(e){
+    const passwordVal=password.value;
+    const confirmPasswordVal=confirmPassword.value;
+    if (passwordVal != confirmPasswordVal) {
+        e.preventDefault();
+        e.stopPropagation();
+        alert("Passwords do not match.");
+        confirmPassword.value="";
+        confirmPassword.focus();
+        
+    }
+    return true;
+});
+const loginHere = document.getElementsByClassName("login-link")
+const signupForm = document.getElementsByClassName("signup-form")
+const signupHere = document.getElementsByClassName("signup-link")
+const loginForm = document.getElementsByClassName("login-form")
+loginHere[0].addEventListener('click', () => {
+    signupForm[0].style.animation = "down 1.01s linear";
+    loginForm[0].style.animation = "left 0.5s linear";
+
+    setTimeout(function(){
+        signupForm[0].style.display = "none";
+        loginForm[0].style.display = "block"
+        signupForm[0].style.animation = " ";
+        loginForm[0].style.animation = " ";
+
+
+}, 1000);
+});
+
+signupHere[0].addEventListener('click', () => {
+    loginForm[0].style.animation = "right 1.01s linear";
+    signupForm[0].style.animation = "up 0.5s linear";
+
+
+    setTimeout(function(){
+        loginForm[0].style.display = "none";
+        signupForm[0].style.display = "block"
+        logiForm[0].style.animation = " ";
+        signupForm[0].style.animation = " ";
+
+}, 800);
+});
---
+++ src/models/User.js
@@ -0,0 +1,93 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+//const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs')
+const utilities = require('../utilities/Utilities')
+const { isEmail, isMobilePhone } = require('validator')
+require('dotenv').config()
+
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        email: {
+            type: String,
+            trim: true,
+            required: true,
+            unique: true,
+            validate: [isEmail, 'Email is Invalid'],
+        },
+        active: {
+            type: Boolean,
+            default: false,
+        },
+        password: {
+            type: String,
+            trim: true,
+            required: true,
+            minlength: 8,
+            validate: [
+                (val) => {
+                    var strength = utilities.checkPasswordStrength(val)
+                    return strength >= 4
+                },
+                'The password must contain a mix of uppercase and lowercase alphabets along with numbers and special chacracters',
+            ],
+        },
+        phoneNumber: {
+            type: String,
+            trim: true,
+            required: true,
+            validate: [isMobilePhone, 'Phone Number is Invalid'],
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+// static method to login user
+userSchema.statics.login = async function (email, password) {
+    const user = await this.findOne({ email })
+    if (user) {
+        const auth = await bcrypt.compare(password, user.password)
+        if (auth) {
+            return user
+        }
+        throw Error('Invalid Credentials')
+    }
+    throw Error('Invalid Credentials')
+}
+
+//creating token for the user
+/*userSchema.methods.createToken=function(){
+    const user= this
+    const maxAge = 30 * 24 * 60 * 60;
+    const token= jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET, {
+        expiresIn: maxAge
+      });
+      return token
+}*/
+
+//deleting the passsword before sending 
+userSchema.methods.toJSON= function(){
+    const user =this
+    const userObject= user.toObject()
+
+    delete userObject.password
+    return userObject
+}
+
+//To hash the password
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt()
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
